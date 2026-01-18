011bfc2c008734592ad401be57187690fd9cfa07
hatimaitijjou <hatimaitijjou2015@gmail.com>
2026-01-18 12:44:48 +0100

    Delete functions directory

--- functions/auth.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com"
-});
-
-firebase.auth().signInAnonymously().catch(console.error);
-
-// Optional: Google Sign-In for SaaS
